03bcd43cf7f507f7ce500e8616b9ff859c0b523e
TanasovBohdan <72256794+TanasovBohdan@users.noreply.github.com>
2020-11-17 13:54:31 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 80
+
+app.get('/', (req, res) => {
+    res.send('me');
+
+})
+
+app.get('/about', (req, res) => {
+    res.send('you');
+})
+
+app.listen(PORT, () => {
+    console.log('Server has ben started...')
+})
